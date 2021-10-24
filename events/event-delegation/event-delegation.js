9b8594bb3d5e15df1ee4a979546f3425fcd3c773
Rakesh <rakesh.singh2@go-mmt.com>
2021-10-24 15:28:01 +0530

    added stopPropagation

--- events/event-delegation/event-delegation.js
+++ events/event-delegation/event-delegation.js
@@ -1,18 +1,30 @@
+/**
+ https://jasonformat.com/event-delegation-vs-direct-binding/
+*/
+
+/**
+ * textContent:= is all text contained by an element and all its children that are for formatting purposes only.
+ * innerText:= returns all text contained by an element and all its child elements.
+ * innerHtml:= returns all text, including html tags, that is contained by an element.
+ * */
+
 (() => {
   const D = document;
   const listViewContainer = D.getElementById("list-view");
 
-  listViewContainer.addEventListener("click", (e) => {
-    const elm = e.target;
-    // https://stackoverflow.com/a/4878963/6191368
-    if (elm.tagName == "LI") {
-      const headElm = D.querySelector("h2");
-      /**
-       * textContent:= is all text contained by an element and all its children that are for formatting purposes only.
-       * innerText:= returns all text contained by an element and all its child elements.
-       * innerHtml:= returns all text, including html tags, that is contained by an element.
-       * */
-      headElm.innerHTML = elm.textContent;
+  const handleListClick = (e) => {
+    const selectedElm = e.target;
+
+    if (selectedElm === listViewContainer) {
+      e.stopPropagation();
     }
-  });
+    // https://stackoverflow.com/a/4878963/6191368
+    if (selectedElm.tagName == "LI") {
+      const headElm = D.querySelector("h2");
+      headElm.textContent = selectedElm.textContent;
+      e.stopPropagation();
+    }
+  };
+
+  listViewContainer.addEventListener("click", handleListClick);
 })();
